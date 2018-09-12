5b007e1db451922f33aad501ff241366f0c0c471
insil321@hotmail.com <insil321@hotmail.com>
2018-09-12 07:39:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,5 +33,5 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line//
 
 var theFunk = function funkyFunction(){
-  return funkyFunction === "FUNKY!"
+  return theFunk  === "FUNKY!"
 }
